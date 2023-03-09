d35b318ae8128fd80ad702ffd06891fbf115e1d8
Mohammd Jawid <76745982+rizaeian@users.noreply.github.com>
2023-03-09 21:24:02 +0600

    add all files

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Die from "./Die";
+import { useNanoid } from "use-nanoid";
+
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: useNanoid(),
+      });
+    }
+    console.log(newDice);
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
+  ));
+
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
+}
