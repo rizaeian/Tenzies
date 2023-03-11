5d18a982650eb9075db010f23505bc4f4a6929ae
Mohammd Jawid <76745982+rizaeian@users.noreply.github.com>
2023-03-11 11:30:34 +0600

    hold dice

--- App.js
+++ App.js
@@ -4,23 +4,40 @@ import { useNanoid } from "use-nanoid";
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice());
-
+  
+  function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+  
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: true,
-        id: useNanoid(),
-      });
+      newDice.push(
+        generateNewDie()
+      );
     }
-    console.log(newDice);
     return newDice;
   }
 
   function rollDice() {
-    setDice(allNewDice());
+    setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? 
+                die :
+                generateNewDie()
+        }))
   }
+  
+  function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
 
   const diceElements = dice.map((die) => (
     <Die key={die.id} value={die.value} isHeld={isHeld} />
